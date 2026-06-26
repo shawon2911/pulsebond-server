afb2067a92af2ce566f9e01794812a6b4c6473bc
shawon2911 <hosenuzzaman2018@gmail.com>
2026-06-26 20:21:53 +0600

    succesfully created api for update user data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
+
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +17,67 @@ app.use(
   }),
 );
 app.use(express.json());
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+   
+    await client.connect();
+
+    const db = client.db("pulse-bond");
+    const userCollection = db.collection("user");
+
+    app.patch("/dashboard/profile-update", async (req, res) => {
+      try {
+        const { email, name, bloodGroup, district, upazila } = req.body;
+        if (!email) {
+      return res.status(400).send({ message: "Email is required to update profile" });
+        }
+      const filter = { email: email };
+      const updateDoc = {
+      $set: {
+        name,
+        bloodGroup,
+        district,
+        upazila
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    if (result.matchedCount === 0) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    res.send({ success: true, message: "Profile updated successfully!", result });
+    
+      } catch (error) {
+        console.error("Update Error:", error);
+    res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+
+
+
+
+
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Pulse Bond Server Running');
