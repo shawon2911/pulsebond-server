ecda38c8b4fbfa3989f448aca6196a9a25d9adf9
shawon2911 <hosenuzzaman2018@gmail.com>
2026-06-28 12:41:38 +0600

    user profile update part done

--- index.js
+++ index.js
@@ -112,6 +112,11 @@ async function run() {
       }
     });
 
+
+
+    
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
